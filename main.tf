c61f31a4406b9de3b119bb325d82d4bb856b30d9
Temi <170970335+tolaoluniyi@users.noreply.github.com>
2024-05-28 20:54:17 -0400

    main

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
-#Create the API Gateway
+#Amazon Certificate
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "temiolaoluniyi.com"
+  validation_method = "EMAIL"
+
+  validation_option {
+    domain_name       = "temiolaoluniyi.com"
+    validation_domain = "temiolaoluniyi.com"
+  }
+}
+resource "aws_route53_record" "www" {
+  zone_id = var.zone_id
+  name    = "temiolaoluniyi.com"
+  type    = "CNAME"
+  ttl     = 300
+  records = []
+}
